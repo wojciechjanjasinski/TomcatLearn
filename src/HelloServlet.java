f27959d28c65e2efa32f46141d242562510dca9f
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-08-14 07:11:29 +0200

    First commit

---
+++ src/HelloServlet.java
@@ -0,0 +1,14 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("Witaj!");
+    }
+}
